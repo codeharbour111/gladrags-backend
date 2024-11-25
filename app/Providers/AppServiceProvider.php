b121efb3ef5cf300181fca61183563f7e1d617ca
MD Aminuzzaman <rminc0102@gmail.com>
2024-11-25 23:39:14 +0600

    Project setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Livewire\Livewire;
+use App\Core\KTBootstrap;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Update defaultStringLength
+        Builder::defaultStringLength(191);
+
+        KTBootstrap::init();
+
+		Livewire::setUpdateRoute(function ($handle) {
+			return Route::post('/starterkit/metronic/laravel/livewire/update', $handle);
+		});
+    }
+}
