8b4a96d9b5c641da538ed62546ec993b88a5d2a5
MD Aminuzzaman <rminc0102@gmail.com>
2024-12-13 05:51:31 +0600

    Login and register update

--- app/Http/Controllers/GladragsUserController.php
+++ app/Http/Controllers/GladragsUserController.php
@@ -13,47 +13,77 @@ class GladragsUserController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-           
-            'email'   => 'required|email',
-            'password' => 'required|min:6'
-        ]);
+        try{
+            $request->validate([
+            
+                'email'   => 'required|email',
+                'password' => 'required|min:6'
+            ]);
 
-        if (Auth::guard('gladrags_users')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember')))
-        {
-            return response()->json(
-                [
-                    'status'  => 'success',
-                    'message' =>  'Login successfully.'
-                ],201);;
+            // $user = User::where('username', $username)->first();  
+
+            // if (Hash::check(Input::get('password'), $user->password))
+            // {
+            //     // The passwords match...
+            // }
+
+            if (Auth::guard('gladrags_users')
+                    ->attempt(['email' => $request->email, 'password' => $request->password], 
+                $request->get('remember')))
+            {
+                //$user = Auth::user();   
+                //$token = $user->tokens()->where('personal_access_tokens.name', 'remember_token')->first();
+                $user = GladragsUser::where('email', $request->email)->first();
+                return response()->json(
+                    [
+                        'status'  => 'success',
+                        'data' =>  [
+                            'user' => $user
+                        ]
+                    ],201);
+            }
+            else{
+                return response()->json(
+                    [
+                        'status'  => 'error',
+                        'message' =>  'Login failed.'
+                    ],201);
+            }
         }
-
-         return response()->json(
-            [
-                'status'  => 'error',
-                'message' =>  'Login failed.'
-            ],201);
+        catch(Exception $e)
+        {
+            return response()->json($e,500);
+        }
     }
    
 
         protected function register(Request $request)
         {
-            $request->validate([
-                'name'   => 'required',
-                'email'   => 'required|email',
-                'password' => 'required|min:6'
-            ]);
+            try{
+                $request->validate([
+                    'name'   => 'required',
+                    'email'   => 'required|email',
+                    'password' => 'required|min:6'
+                ]);
 
-            $gladrag_user = GladragsUser::create([
-                'name' => $request['name'],
-                'email' => $request['email'],
-                'password' => Hash::make($request['password']),
-            ]);
+                $gladrag_user = GladragsUser::create([
+                    'name' => $request['name'],
+                    'email' => $request['email'],
+                    'password' => Hash::make($request['password']),
+                ]);
 
-              return response()->json(
-                [
-                    'status'  => 'success',
-                    'message' =>  'Registered successfully.'
-                ],201);
+                return response()->json(
+                    [
+                        'status'  => 'success',
+                        'data' =>  [
+                            'user' => $gladrag_user
+                        ]
+                    ],201);
+
+            }
+            catch(Exception $e)
+            {
+                return response()->json($e,500);
+            }
         }
 }
--- app/Models/GladragsUser.php
+++ app/Models/GladragsUser.php
@@ -17,7 +17,7 @@ class GladragsUser extends Authenticatable
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'created_at', 'updated_at'
     ];
 
 }
