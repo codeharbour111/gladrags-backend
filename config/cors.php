d734e271300057b8470ae903b6467e396ee6054a
MD Aminuzzaman <rminc0102@gmail.com>
2025-01-24 02:11:22 +0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','https://gladragsbd.netlify.app'],
+    'allowed_origins' => ['http://localhost:3000','https://gladragsbd.netlify.app', 'https://gladragsinc.com'],
     // 'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
